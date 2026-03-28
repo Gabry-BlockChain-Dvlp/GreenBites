7ad997d2c21ec5eeabb7de35780f23e03555e0bd
Gabriele Amoruso <gabryanoru@gmail.com>
2026-03-28 16:14:26 +0100

    quick fix

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,8 +3,10 @@ import "../css/hero.css";
 function Hero() {
     return (
         <div className="hero">
-            <h1>Welcome to GreenBites!</h1>
-            <p>Your go-to app for delicious vegetarian recipes.</p>
+            <div className="hero-description">
+             <h1>Welcome to GreenBites!</h1>
+             <p>Your go-to app for delicious vegetarian recipes.</p>
+            </div>
         </div>
     );
 };
